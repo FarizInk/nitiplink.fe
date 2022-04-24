c083889203e958a8fbd6bfe5d00c6636a14d335f
Fariz <nizaralfariziakbar10@gmail.com>
2022-04-24 16:42:47 +0700

    enable dark mode switcher

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,17 +1,27 @@
 <script>
 	import Navbar from '$lib/navbar/Navbar.svelte';
 	import '../app.css';
-	import "flowbite/dist/flowbite.css";
+	import 'flowbite/dist/flowbite.css';
 	import Footer from '$lib/Footer.svelte';
+	import { onMount } from 'svelte';
+	import { changeTheme } from '$lib/helper';
+	import { theme } from '$lib/stores';
+
+	onMount(async () => {
+		if (localStorage.theme === undefined || localStorage.theme === null) {
+			localStorage.theme = 'light';
+			theme.set('light');
+		}
+		changeTheme();
+	});
 </script>
 
-
-<div class="flex flex-col h-screen justify-between">
+<div class="flex flex-col h-screen justify-between bg-white dark:bg-gray-800">
 	<Navbar />
 
-	<main class="mb-auto h-auto mt-16">
+	<main class="mb-auto h-auto mt-16 text-black dark:text-white">
 		<slot />
 	</main>
-	
-	<Footer/>
+
+	<Footer />
 </div>

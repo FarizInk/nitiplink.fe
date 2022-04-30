a877dfb1519c679625b98e86347504af10222b87
Fariz <nizaralfariziakbar10@gmail.com>
2022-05-01 00:15:03 +0700

    chenge modal auth

--- src/lib/navbar/Navbar.svelte
+++ src/lib/navbar/Navbar.svelte
@@ -1,13 +1,12 @@
 <script>
 	import { page } from '$app/stores';
+	import Modal from '$lib/components/Modal.svelte';
 	import { changeTheme } from '$lib/helper';
-	import ModalSignIn from '$lib/modal/SignIn.svelte';
-	import ModalSignUp from '$lib/modal/SignUp.svelte';
 	import { theme, token } from '$lib/stores';
 
 	const activeMenu =
 		'block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white';
-		// 'block py-2 pr-4 pl-3 md:p-0 animate-move-bg bg-gradient-to-r from-indigo-500 via-pink-500 to-indigo-500 bg-[length:400%] bg-clip-text text-transparent';
+	// 'block py-2 pr-4 pl-3 md:p-0 animate-move-bg bg-gradient-to-r from-indigo-500 via-pink-500 to-indigo-500 bg-[length:400%] bg-clip-text text-transparent';
 	const baseMenu =
 		'block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700';
 
@@ -43,18 +42,19 @@
 					fill={themeVal === 'dark' ? 'white' : 'black'}
 				/>
 			</svg>
-			<span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">NitipLink</span
-			>
+			<span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
+				NitipLink
+			</span>
 		</a>
 
 		<div class="flex items-center md:order-2">
 			{#if authToken === null}
-			<button
-				type="button"
-				class="btn-primary btn-xs bg-blue-600 mr-3"
-				on:click={handleSignInBtnShow}>Sign In</button
-			>
-			<button class="btn-secondary btn-xs mr-3" on:click={handleSignUpBtnShow}>Sign Up</button>
+				<button
+					type="button"
+					class="btn-primary btn-xs bg-blue-600 mr-3"
+					on:click={handleSignInBtnShow}>Sign In</button
+				>
+				<button class="btn-secondary btn-xs mr-3" on:click={handleSignUpBtnShow}>Sign Up</button>
 			{/if}
 
 			<button class="btn-secondary btn-compact mr-3" on:click={toggleTheme}>
@@ -90,93 +90,93 @@
 			</button>
 
 			{#if authToken !== null}
-			<button
-				type="button"
-				class="flex md:mr-3 text-sm bg-gray-800 rounded-full focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600"
-				id="user-menu-button"
-				aria-expanded="false"
-				data-dropdown-toggle="dropdown-user"
-			>
-				<span class="sr-only">Open user menu</span>
-				<img
-					class="w-8 h-8 rounded-full"
-					src="https://ui-avatars.com/api/?background=random&name=Fariz"
-					alt="user photo"
-				/>
-			</button>
-			<!-- Dropdown menu -->
-			<div
-				class="hidden z-50 my-4 text-base list-none bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700 dark:divide-gray-600"
-				id="dropdown-user"
-			>
-				<div class="py-3 px-4">
-					<span class="block text-sm text-gray-900 dark:text-white">Bonnie Green</span>
-					<span class="block text-sm font-medium text-gray-500 truncate dark:text-gray-400"
-						>name@flowbite.com</span
-					>
+				<button
+					type="button"
+					class="flex md:mr-3 text-sm bg-gray-800 rounded-full focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600"
+					id="user-menu-button"
+					aria-expanded="false"
+					data-dropdown-toggle="dropdown-user"
+				>
+					<span class="sr-only">Open user menu</span>
+					<img
+						class="w-8 h-8 rounded-full"
+						src="https://ui-avatars.com/api/?background=random&name=Fariz"
+						alt="user photo"
+					/>
+				</button>
+				<!-- Dropdown menu -->
+				<div
+					class="hidden z-50 my-4 text-base list-none bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700 dark:divide-gray-600"
+					id="dropdown-user"
+				>
+					<div class="py-3 px-4">
+						<span class="block text-sm text-gray-900 dark:text-white">Bonnie Green</span>
+						<span class="block text-sm font-medium text-gray-500 truncate dark:text-gray-400"
+							>name@flowbite.com</span
+						>
+					</div>
+					<ul class="py-1" aria-labelledby="dropdown-user">
+						<li>
+							<a
+								href="#"
+								class="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+								>Dashboard</a
+							>
+						</li>
+						<li>
+							<a
+								href="#"
+								class="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+								>Settings</a
+							>
+						</li>
+						<li>
+							<a
+								href="#"
+								class="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+								>Earnings</a
+							>
+						</li>
+						<li>
+							<a
+								href="#"
+								class="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+								>Sign out</a
+							>
+						</li>
+					</ul>
 				</div>
-				<ul class="py-1" aria-labelledby="dropdown-user">
-					<li>
-						<a
-							href="#"
-							class="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
-							>Dashboard</a
-						>
-					</li>
-					<li>
-						<a
-							href="#"
-							class="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
-							>Settings</a
-						>
-					</li>
-					<li>
-						<a
-							href="#"
-							class="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
-							>Earnings</a
-						>
-					</li>
-					<li>
-						<a
-							href="#"
-							class="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
-							>Sign out</a
-						>
-					</li>
-				</ul>
-			</div>
-			<button
-				data-collapse-toggle="mobile-menu-2"
-				type="button"
-				class="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-				aria-controls="mobile-menu-2"
-				aria-expanded="false"
-			>
-				<span class="sr-only">Open main menu</span>
-				<svg
-					class="w-6 h-6"
-					fill="currentColor"
-					viewBox="0 0 20 20"
-					xmlns="http://www.w3.org/2000/svg"
-					><path
-						fill-rule="evenodd"
-						d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-						clip-rule="evenodd"
-					/></svg
+				<button
+					data-collapse-toggle="mobile-menu-2"
+					type="button"
+					class="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+					aria-controls="mobile-menu-2"
+					aria-expanded="false"
 				>
-				<svg
-					class="hidden w-6 h-6"
-					fill="currentColor"
-					viewBox="0 0 20 20"
-					xmlns="http://www.w3.org/2000/svg"
-					><path
-						fill-rule="evenodd"
-						d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-						clip-rule="evenodd"
-					/></svg
-				>
-			</button>
+					<span class="sr-only">Open main menu</span>
+					<svg
+						class="w-6 h-6"
+						fill="currentColor"
+						viewBox="0 0 20 20"
+						xmlns="http://www.w3.org/2000/svg"
+						><path
+							fill-rule="evenodd"
+							d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+							clip-rule="evenodd"
+						/></svg
+					>
+					<svg
+						class="hidden w-6 h-6"
+						fill="currentColor"
+						viewBox="0 0 20 20"
+						xmlns="http://www.w3.org/2000/svg"
+						><path
+							fill-rule="evenodd"
+							d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+							clip-rule="evenodd"
+						/></svg
+					>
+				</button>
 			{/if}
 		</div>
 
@@ -205,5 +205,46 @@
 	</div>
 </nav>
 
-<ModalSignUp bind:this={modalSignUp} />
-<ModalSignIn bind:this={modalSignIn} />
+<Modal bind:this={modalSignIn} id="modal-sign-in" type="popup" size="sm">
+	<form class="space-y-6" action="#" slot="body">
+		<h3 class="text-xl font-medium text-gray-900 dark:text-white">Sign In</h3>
+		<div>
+			<label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your email</label>
+			<input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="name@company.com" required>
+		</div>
+		<div>
+			<label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your password</label>
+			<input type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required>
+		</div>
+		<div class="flex justify-between">
+			<div class="flex items-start">
+				<div class="flex items-center h-5">
+					<input id="remember" aria-describedby="remember" type="checkbox" class="w-4 h-4 bg-gray-50 rounded border border-gray-300 focus:ring-3 focus:ring-blue-300 dark:bg-gray-600 dark:border-gray-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800" required>
+				</div>
+				<div class="ml-3 text-sm">
+				<label for="remember" class="font-medium text-gray-900 dark:text-gray-300">Remember me</label>
+				</div>
+			</div>
+			<a href="#" class="text-sm text-blue-700 hover:underline dark:text-blue-500">Lost Password?</a>
+		</div>
+		<button type="submit" class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Login to your account</button>
+		<div class="text-sm font-medium text-gray-500 dark:text-gray-300">
+			Not registered? <a href="#" class="text-blue-700 hover:underline dark:text-blue-500" on:click={() => modalSignUp.show()}>Create account</a>
+		</div>
+	</form>
+</Modal>
+
+<Modal bind:this={modalSignUp} id="modal-sign-up" type="popup" size="sm" zIndex="z-[52]" backdropIndex="z-[51]">
+	<form class="space-y-6" action="#" slot="body">
+		<h3 class="text-xl font-medium text-gray-900 dark:text-white">Sign Up</h3>
+		<div>
+			<label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your email</label>
+			<input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="name@company.com" required>
+		</div>
+		<div>
+			<label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your password</label>
+			<input type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required>
+		</div>
+		<button type="submit" class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Register</button>
+	</form>
+</Modal>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -57,12 +57,19 @@
 
 <button on:click={showModalOne} class="btn-secondary btn-sm ml-3">Show modal 1</button>
 
-<Modal bind:this={modalLvTwo} id="modal-lv-two" position="center-center" size="sm" zIndex="z-[52]" backdropIndex="z-[51]">
-	End Of Nested Modal
+<Modal bind:this={modalLvTwo} id="modal-lv-two" position="center-center" size="sm" zIndex="z-[52]" backdropIndex="z-[51]" type="popup">
+	<div class="p-6 text-center" slot="body">
+		<svg class="mx-auto mb-4 w-14 h-14 text-gray-400 dark:text-gray-200" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+		<h3 class="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">Are you sure you want to delete this product?</h3>
+		<button data-modal-toggle="popup-modal" type="button" class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center mr-2">
+			Yes, I'm sure
+		</button>
+		<button data-modal-toggle="popup-modal" type="button" class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">No, cancel</button>
+	</div>
 </Modal>
 
-<Modal bind:this={modalLvOne} id="modal-lv-one" position="center-center" size="lg">
-	<button on:click={showModalTwo} class="btn-primary btn-sm">Show modal 2</button>
+<Modal bind:this={modalLvOne} id="modal-lv-one" position="center-center" size="lg" footer={false} title="Test Modal">
+	<div slot="body">
+		<button on:click={showModalTwo} class="btn-primary btn-sm">Show modal 2</button>
+	</div>
 </Modal>
-
-
